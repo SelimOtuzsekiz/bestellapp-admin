1ad18d63afb9ea0c761af9144fe18fac8c7d04cd
talha <talha.demirel@gmx.de>
2022-02-10 14:48:19 +0100

    push

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Header from "./components/Header/Header";
 import Content from "./components/Content/Content";
 
 function App() {
-  localStorage.setItem('Auth', 'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWZkNTFlMGY5NWUxM2Y1OWNhZjdkMzYiLCJpYXQiOjE2NDM5OTE1NTEsImV4cCI6MTY0NDAyNzU1MX0.eyDn3EYbNnhaW-kyE8IHfSSwOToWweJKTg3AHxC5rgE');
+  localStorage.setItem('Auth', 'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWZkNTFlMGY5NWUxM2Y1OWNhZjdkMzYiLCJpYXQiOjE2NDQ0OTc2NjUsImV4cCI6MTY0NDUzMzY2NX0.K6crLrVZT-w2M4CFeUdmgyTyTdcVdKcV0iTBtUbyxC8');
   
   const[status,setStatus] = useState("offen");
 
